e0f09e093394ffec9b9cdba67093054dcd24959f
lendale <allen@usjr.edu.ph>
2016-05-21 15:43:53 +0800

    *Added Butterknife Dependency

---
+++ app/src/main/java/com/gabo/musika/MusikaApp.java
@@ -0,0 +1,13 @@
+package com.gabo.musika;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 5/21/2016.
+ */
+public class MusikaApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
